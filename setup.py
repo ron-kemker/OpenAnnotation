44c3f5649ce46d9bbfa9c04e07fbe80454ad023c
Ronald Kemker <63465692+ron-kemker@users.noreply.github.com>
2021-01-31 15:30:20 -0500

    Added setup.py
    This needs to be tested prior to getting pushed.

---
+++ setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 31 15:21:25 2021
+
+@author: Ronald Kemker
+"""
+
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+       
+setuptools.setup(
+    name="openannotation-ron-kemker", # Replace with your own username
+    version="0.1.0",
+    author="Ronald Kemker",
+    author_email="rmkemker@mtu.edu",
+    description="Open-Source Annotation Tool for Computer Vision Projects",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ron-kemker/OpenAnnotation",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    # python_requires='>=3.6',
+)

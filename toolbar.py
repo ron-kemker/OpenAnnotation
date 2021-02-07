b087479e537077c1126ebf620d566a5d45a14403
Ronald Kemker <63465692+ron-kemker@users.noreply.github.com>
2021-02-06 20:06:10 -0500

    Update toolbar.py
    Updates required for new Annotation object in fileIO.py

--- toolbar.py
+++ toolbar.py
@@ -200,7 +200,8 @@ class Toolbar(object):
     def _delete_from_project(self):
         
         for lbl in self.root_app.annotations[self.root_app.current_file].label:
-            self.root_app.class_count[lbl] = self.root_app.class_count[lbl] - 1
+            idx = self.class_list.index(lbl)
+            self.root_app.class_count[idx] = self.root_app.class_count[idx] - 1
         
         self.root_app.annotations.pop(self.root_app.current_file)
         self.root_app.file_list.pop(self.root_app.current_file)

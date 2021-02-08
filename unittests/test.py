d29c17ca247bdee587b894ef5e6bfc6a8d4081ba
Ronald Kemker <63465692+ron-kemker@users.noreply.github.com>
2021-02-08 18:38:54 -0500

    Create test.py
    Adding Generic Test Script to go through all of test cases.

---
+++ unittests/test.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 30 09:06:03 2021
+
+@author: Ronald Kemker
+"""
+
+from AnnotationTool_test import TestAnnotationTool
+from menu_test import TestMenu
+import unittest
+
+
+
+
+
+
+if __name__ == '__main__':
+    # Unit Tests
+    TestAnnotationTool()
+    TestMenu()
+    unittest.main()
